a069849afa6f6095ddd4b6d8034c2e87f81e9db0
Frankie <franco.mastrantonio@davinci.edu.ar>
2024-04-25 20:31:18 -0300

    conectadas las apps

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import * as project from '../services/projectsServ'
+import envParams from '../envParams.json'
 //import { EqParams, ChannelConfig, SoundListItem, States } from '../types'
 
 const router = express.Router()
-
+const frontendEndpoint: string = envParams.dev['front-endpoint-access-control'] as string
+console.log(frontendEndpoint)
 router.get('/:id', (req, res) => {
     console.log('request project')
     const resProject = project.getProject(+req.params.id)
+    res.header("Access-Control-Allow-Origin", frontendEndpoint)
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     res.send(resProject)
     console.log(res)
 })
