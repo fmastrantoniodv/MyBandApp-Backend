7eb7cedc369834e69a0fc1ec842f672d5683127f
Frankie <franco.mastrantonio@davinci.edu.ar>
2024-05-07 22:43:57 -0300

    Se agrega delete fav

--- src/routes/favourites.ts
+++ src/routes/favourites.ts
@@ -27,13 +27,27 @@ try{
         userId,
         newFavEntry 
     )
-
+    if(newFav === false){
+        throw new Error('No se agregar el nuevo fav porque ya existe')
+    }
     res.json(newFav)
 }catch (e: any) {
     res.status(400).send(e.message)
 }
+})
 
-
+router.delete('/', (req, res) => {
+    try{
+        const { userId, sampleId } = req.body
+        parseUserId(userId)
+        parseStringFromRequest(sampleId)
+        if(!favouritesSamplesServ.deleteFav(userId, sampleId)){
+            throw new Error('No se pudo borrar')
+        }
+        res.send("ok!")
+    }catch (e: any) {
+        res.status(400).send(e.message)
+    }
 })
 
 export default router
--- src/services/favouritesSamplesServ.ts
+++ src/services/favouritesSamplesServ.ts
@@ -7,9 +7,26 @@ export const getFavouritesList = (): Array<SampleFav> | undefined => {
     return favouritesList
 }
 
-export const addNewFav = (userId: number, favEntry: SampleFav): SampleFav => {
+export const addNewFav = (userId: number, favEntry: SampleFav): SampleFav | boolean => {
     console.log('fav from userId:',userId)
     console.log('fav:', favEntry)
-    favouritesList.push(favEntry)
-    return favEntry
+    var indexFav = favouritesList.findIndex(value => value.sampleId === favEntry.sampleId)
+    if(indexFav < 0){
+        favouritesList.push(favEntry)    
+        return favEntry
+    }else{
+        return false
+    }
+}
+
+export const deleteFav = (userId: number, sampleId: string): boolean => {
+    console.log('fav from userId:',userId)
+    console.log('fav sampleId:', sampleId)
+    var indexDeleteFav = favouritesList.findIndex(value => value.sampleId === sampleId)
+    if(indexDeleteFav >= 0){
+        favouritesList.splice(indexDeleteFav, 1)
+        return true
+    }else{
+        return false
+    }
 }
